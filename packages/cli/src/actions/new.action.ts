b432eacf636a919db8a16b7d2829dd13fa18dca5
“张智皓” <zhang948498@163.com>
2021-10-22 17:14:55 +0800

    fix .gitignore

--- packages/cli/src/actions/new.action.ts
+++ packages/cli/src/actions/new.action.ts
@@ -1,11 +1,12 @@
 import { AbstractAction } from './abstract.action';
-import { ensureDir, copy, readJson, writeJson } from 'fs-extra';
+import { ensureDir, copy, readJson, writeJson, writeFile } from 'fs-extra';
 import * as inquirer from 'inquirer';
 import { Input } from '../commands';
 import { join } from 'path';
 import ora from 'ora';
 import chalk from 'chalk';
 import { EMOJIS } from '../ui';
+import { defaultGitIgnore } from '../utils/defaults';
 export class NewAction extends AbstractAction {
 	public async handle(inputs: Input[], options: Input[]) {
 		const root = process.cwd();
@@ -40,6 +41,8 @@ export class NewAction extends AbstractAction {
 			const packageJson = await readJson(join(root, appName, 'package.json'));
 			packageJson.name = appName;
 			await writeJson(join(root, appName, 'package.json'), packageJson, { spaces: 2 });
+
+			await createGitIgnoreFile(join(root, appName));
 			spinner.succeed();
 			console.info(chalk.green('Creating a successful  =>  ', appName));
 		} catch (e: any) {
@@ -50,3 +53,19 @@ export class NewAction extends AbstractAction {
 		// process.exit(0);
 	}
 }
+
+/**
+ * Write a file `.gitignore` in the root of the newly created project.
+ * `.gitignore` available in `@nestjs/schematics` cannot be published to
+ * NPM (needs to be investigated).
+ *
+ * @param url
+ * @param content (optional) Content written in the `.gitignore`.
+ *
+ * @return Resolves when succeeds, or rejects with any error from `fn.writeFile`.
+ */
+const createGitIgnoreFile = (url: string, content?: string) => {
+	const fileContent = content || defaultGitIgnore;
+	const filePath = join(url, '.gitignore');
+	return writeFile(filePath, fileContent);
+};
