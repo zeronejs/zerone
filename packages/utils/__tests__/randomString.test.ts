a3bea8f03b5a6a7667fd1000ad463846ac443ab2
“张智皓” <zhang948498@163.com>
2021-10-30 11:38:51 +0800

    fix: 🐛 fix jest cov

--- packages/utils/__tests__/randomString.test.ts
+++ packages/utils/__tests__/randomString.test.ts
@@ -2,8 +2,8 @@ import { randomChars, randomNumChars, randomStrChars } from '../src/randomString
 describe('@zeronejs/utils => randomString', () => {
 	it('随机字符串', async () => {
 		const randomStringLength = 32;
-		expect(randomChars(randomStringLength)).toHaveLength(randomStringLength);
-		expect(randomNumChars(randomStringLength)).toHaveLength(randomStringLength);
-		expect(randomStrChars(randomStringLength)).toHaveLength(randomStringLength);
+		expect(randomChars()).toHaveLength(randomStringLength);
+		expect(randomNumChars()).toHaveLength(randomStringLength);
+		expect(randomStrChars()).toHaveLength(randomStringLength);
 	});
 });
