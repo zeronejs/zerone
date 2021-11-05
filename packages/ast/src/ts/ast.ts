d7f2aade898ae82f80c4e33f024230768e716602
“张智皓” <zhang948498@163.com>
2021-11-05 17:50:30 +0800

    feat: 🎸 start generateDecoratorDoc

--- packages/ast/src/ts/ast.ts
+++ packages/ast/src/ts/ast.ts
@@ -2,12 +2,12 @@
 
 import { basename } from 'path';
 import * as ts from 'typescript';
-// import * as fs from 'fs';
 
 export interface DocEntry {
 	// path: string;
 	fileName: string;
 	inports: FileImports[];
+	classes: FileClasses[];
 }
 export interface FileImports {
 	from: string;
@@ -15,6 +15,10 @@ export interface FileImports {
 	defalutImport?: string;
 	namespaceImport?: string;
 }
+export interface FileClasses {
+	name: string;
+}
+
 /** Generate documentation for all classes in a set of .ts files */
 export function generateDocumentation(
 	fileNames: string[],
@@ -33,42 +37,13 @@ export function generateDocumentation(
 		// todo
 		const { importDeclarations, variableStatements, enumDeclarations, classDeclarations } =
 			getDeclarations(sourceFile);
-		const inports: FileImports[] = [];
-		for (const importDeclaration of importDeclarations) {
-			const fileImport: FileImports = {
-				from: '',
-				elements: [],
-				defalutImport: undefined,
-				namespaceImport: undefined,
-			};
-			if (ts.isStringLiteral(importDeclaration.moduleSpecifier)) {
-				fileImport.from = importDeclaration.moduleSpecifier.text;
-			}
-
-			if (importDeclaration.importClause?.name?.escapedText) {
-				// ts.unescapeLeadingUnderscores Remove extra underscore from escaped identifier text content.
-				fileImport.defalutImport = ts.unescapeLeadingUnderscores(
-					importDeclaration.importClause.name.escapedText
-				);
-			}
-			if (importDeclaration.importClause?.namedBindings) {
-				if (ts.isNamedImports(importDeclaration.importClause.namedBindings)) {
-					fileImport.elements = importDeclaration.importClause.namedBindings.elements.map((it) =>
-						ts.unescapeLeadingUnderscores(it.name.escapedText)
-					);
-				} else if (ts.isNamespaceImport(importDeclaration.importClause.namedBindings)) {
-					fileImport.namespaceImport = ts.unescapeLeadingUnderscores(
-						importDeclaration.importClause.namedBindings.name.escapedText
-					);
-				}
-			}
-
-			inports.push(fileImport);
-		}
+		const inports = generateImportDeclarationsDoc(importDeclarations);
+		const classes = generateClassDeclarationsDoc(classDeclarations);
 		return {
 			// path: sourceFile.fileName,
 			fileName: basename(sourceFile.fileName),
 			inports,
+			classes,
 		};
 	}
 }
@@ -80,6 +55,90 @@ export function getSourceFiles(fileNames: string[], options: ts.CompilerOptions)
 	const sources = program.getSourceFiles();
 	// 过滤ts自带声明(.d.ts)文件 和 无关文件
 	return sources.filter((it) => !it.isDeclarationFile && fileNames.includes(it.fileName));
+}
+export function generateImportDeclarationsDoc(importDeclarations: ts.ImportDeclaration[]): FileImports[] {
+	const inports: FileImports[] = [];
+	for (const importDeclaration of importDeclarations) {
+		const fileImport: FileImports = {
+			from: '',
+			elements: [],
+			defalutImport: undefined,
+			namespaceImport: undefined,
+		};
+		if (ts.isStringLiteral(importDeclaration.moduleSpecifier)) {
+			fileImport.from = importDeclaration.moduleSpecifier.text;
+		}
+
+		if (importDeclaration.importClause?.name?.escapedText) {
+			// ts.unescapeLeadingUnderscores Remove extra underscore from escaped identifier text content.
+			fileImport.defalutImport = ts.unescapeLeadingUnderscores(
+				importDeclaration.importClause.name.escapedText
+			);
+		}
+		if (importDeclaration.importClause?.namedBindings) {
+			if (ts.isNamedImports(importDeclaration.importClause.namedBindings)) {
+				fileImport.elements = importDeclaration.importClause.namedBindings.elements.map((it) =>
+					ts.unescapeLeadingUnderscores(it.name.escapedText)
+				);
+			} else if (ts.isNamespaceImport(importDeclaration.importClause.namedBindings)) {
+				fileImport.namespaceImport = ts.unescapeLeadingUnderscores(
+					importDeclaration.importClause.namedBindings.name.escapedText
+				);
+			}
+		}
+
+		inports.push(fileImport);
+	}
+	return inports;
+}
+export function generateClassDeclarationsDoc(classDeclarations: ts.ClassDeclaration[]): FileClasses[] {
+	const classes: FileClasses[] = [];
+	for (const classDeclaration of classDeclarations) {
+		const fileClasses: FileClasses = {
+			name: '',
+		};
+		if (classDeclaration.name?.escapedText) {
+			fileClasses.name = ts.unescapeLeadingUnderscores(classDeclaration.name.escapedText);
+		}
+		const decorators = generateDecoratorDoc(classDeclaration.decorators);
+		classes.push(fileClasses);
+	}
+	return classes;
+}
+export interface DecoratorDoc {
+	name: string;
+	expression?: DecoratorExpressionDoc;
+}
+export interface DecoratorExpressionDoc {
+	args: any[];
+	expression?: DecoratorExpressionDoc;
+}
+export function generateDecoratorDoc(decorators?: ts.NodeArray<ts.Decorator>) {
+	if (!decorators) return [];
+	return decorators.map((ItemDecorator) => {
+		const decorator = {
+			name: '',
+			expression: { args: [] },
+		};
+		// todo. This assumes that the decorator does not have multiple expressions nested
+		recursiveExpression(ItemDecorator.expression, decorator.expression);
+		debugger
+		function recursiveExpression(
+			expression: ts.LeftHandSideExpression,
+			putExpression: DecoratorExpressionDoc
+		) {
+			if (ts.isIdentifier(expression)) {
+				decorator.name = ts.unescapeLeadingUnderscores(expression.escapedText);
+			} else if (ts.isCallExpression(expression)) {
+				const args = expression.arguments.map((arg) => {
+					////////////// See you next week
+				});
+				putExpression.args = args;
+				putExpression.expression = { args: [] };
+				recursiveExpression(expression.expression, putExpression.expression);
+			}
+		}
+	});
 }
 function getDeclarations(sourceFile: ts.SourceFile) {
 	const importDeclarations: ts.ImportDeclaration[] = [];
