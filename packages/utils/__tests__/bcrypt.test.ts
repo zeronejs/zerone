98c3af8da82d77342e3ec207fd640290ffdd02a6
“张智皓” <zhang948498@163.com>
2021-11-15 15:51:09 +0800

    fix: 🐛 remove bcrypt

--- packages/utils/__tests__/bcrypt.test.ts
+++
@@ -1,8 +0,0 @@
-import { encryptedUserPassword, verifyUserPassword } from '../src/crypto';
-describe('@zeronejs/utils => crypto', () => {
-	it('密码加密验证', async () => {
-		const password = '123456';
-		const cryptoed = await encryptedUserPassword(password);
-		expect(await verifyUserPassword(password, cryptoed)).toBeTruthy();
-	});
-});
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,2 @@
-export * from './crypto';
 export * from './randomString';
 export * from './array';
