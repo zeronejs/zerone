50a52156942614a8818340bdcf2384dc48d9ef7e
“张智皓” <zhang948498@163.com>
2021-12-01 15:49:31 +0800

    feat: 🎸 wechat login beta.5

--- packages/wechat/login/src/miniProgram/miniProgram.service.ts
+++ packages/wechat/login/src/miniProgram/miniProgram.service.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { WXBizDataCrypt } from '@zeronejs/utils';
 import {
-	WechatLoginMiniProgramServiceGetUserInfo,
+	WechatLoginMiniProgramServiceWxLogin,
 	WechatLoginMiniProgramServiceGetSessionKey,
+	WechatLoginMiniProgramServiceGetSessionKeyResult,
+	WechatLoginMiniProgramServiceWxLoginResult,
+	WechatLoginMiniProgramServiceGetUserProfile,
+	WechatLoginMiniProgramServiceGetUserProfileResult,
 } from './miniProgram.types';
 /**
  * 小程序登录
@@ -11,20 +15,44 @@ import {
  */
 @Injectable()
 export class WechatLoginMiniProgramService {
-	async getUserInfo(input: WechatLoginMiniProgramServiceGetUserInfo) {
-		const sessionKey = await this.getSessionKey(input);
-		const pc = new WXBizDataCrypt(input.appId, sessionKey);
+	/**
+	 * jscode2session  getUserProfile 的合集
+	 */
+	async wxLogin(
+		input: WechatLoginMiniProgramServiceWxLogin
+	): Promise<WechatLoginMiniProgramServiceWxLoginResult> {
+		const sessionKeyObj = await this.jscode2session(input);
+		const { openid, unionid } = sessionKeyObj;
+		const data = this.getUserProfile({
+			...input,
+			session_key: sessionKeyObj.session_key,
+		});
+		return { ...data, unionid, openid };
+	}
+	/**
+	 * encryptedData解密
+	 */
+	getUserProfile(
+		input: WechatLoginMiniProgramServiceGetUserProfile
+	): WechatLoginMiniProgramServiceGetUserProfileResult {
+		const pc = new WXBizDataCrypt(input.appId, input.session_key);
 		const data = pc.decryptData(input.encryptedData, input.iv);
 		return data;
 	}
-	async getSessionKey(input: WechatLoginMiniProgramServiceGetSessionKey) {
-		const { status, data } = await axios({
-			method: 'get',
-			url: `https://api.weixin.qq.com/sns/jscode2session?appid=${input.appId}&secret=${input.secret}&js_code=${input.code}&grant_type=authorization_code`,
-		});
+	/**
+	 * 获取session_key
+	 */
+	async jscode2session(input: WechatLoginMiniProgramServiceGetSessionKey) {
+		const { status, data } = await axios.get<WechatLoginMiniProgramServiceGetSessionKeyResult>(
+			`https://api.weixin.qq.com/sns/jscode2session?appid=${input.appId}&secret=${input.secret}&js_code=${input.code}&grant_type=authorization_code`
+		);
 		if (status !== 200) {
-			throw new Error('网络错误，请重试');
+			throw { errcode: -1, errmsg: '系统繁忙' };
 		}
-		return data.session_key;
+		const { session_key, openid, unionid } = data;
+		if (!session_key || !openid) {
+			throw data;
+		}
+		return { session_key, openid, unionid };
 	}
 }
