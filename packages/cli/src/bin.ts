320cb2c941eb8498ad1657903f1fce1f5675e439
“张智皓” <zhang948498@163.com>
2021-12-03 17:14:56 +0800

    fix: 🐛 package.json 路径

--- packages/cli/src/bin.ts
+++ packages/cli/src/bin.ts
@@ -3,10 +3,15 @@ import { Command } from 'commander';
 import { CommandLoader } from './commands';
 // import { loadLocalBinCommandLoader, localBinExists } from '../lib/utils/local-binaries';
 import { readJSONSync } from 'fs-extra';
+import { join } from 'path';
 const bootstrap = () => {
     const program = new Command();
     program
-        .version(readJSONSync('../package.json').version, '-v, --version', 'Output the current version.')
+        .version(
+            readJSONSync(join(__dirname, '../package.json')).version,
+            '-v, --version',
+            'Output the current version.'
+        )
         .usage('<command> [options]')
         .helpOption('-h, --help', 'Output usage information.');
 
