8b87b75d29750485229904c2a588044f6531679c
张智皓 <zhang948498@163.com>
2022-09-22 15:50:45 +0800

    fix: 🐛 HttpExceptionFilter typeorm补充

--- packages/cli/templates/new/src/common/filters/httpException.filter.ts
+++ packages/cli/templates/new/src/common/filters/httpException.filter.ts
@@ -1,16 +1,28 @@
 import { RDto } from '@common/Result.dto';
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { isString } from '@zeronejs/utils';
-import { Request, Response } from 'express';
+import { Response } from 'express';
+import { QueryFailedError } from 'typeorm';
 
-@Catch(HttpException)
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
+    catch(exception: unknown, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
         // const request = ctx.getRequest<Request>();
-        const status = exception.getStatus();
-        const excRes = exception.getResponse();
+        let status: number;
+        let excRes: string | object;
+        if (exception instanceof HttpException) {
+            status = exception.getStatus();
+            excRes = exception.getResponse();
+        } else if (exception instanceof QueryFailedError) {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            excRes = exception.message;
+        } else {
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            excRes = exception as object;
+        }
+
         let resDto: RDto<null>;
         if (isString(excRes)) {
             resDto = new RDto({ code: status, data: null, msg: excRes });
